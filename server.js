d46ea381f4a7a7df9a4cb7432f9395e7418482b4
josh.horecny <jphorec@gmail.com>
2015-11-03 16:04:55 -0600

    Added images

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/js", express.static(__dirname + '/js'));
+app.use("/images", express.static(__dirname + '/images'));
 app.use("/views/partials", express.static(__dirname + '/views/partials'));
 var db = mongoskin.db('mongodb://dbadmin:dbpassword@ds049744.mongolab.com:49744/heroku_5q9qxh3r', {safe:true})
 // Add headers
