01202fc836ca01b27dd7e1ecce8f29ca19fb68aa
josh.horecny <jphorec@gmail.com>
2015-11-03 15:11:16 -0600

    Added Angular Junk and now working index page

--- routes/restservice.js
+++ routes/restservice.js
@@ -5,23 +5,22 @@ var express = require('express');
 var router = express();
 
 
-router.param('collectionName', function(req, res, next, collectionName){
-    req.collection = db.collection(collectionName)
-    return next()
-})
 
-router.get('/', function(req, res, next) {
+
+router.get('/', function(req, res) {
     res.send('Must Include Collection Name')
 })
 
-router.get('/collections/:collectionName', function(req, res, next) {
-    req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+router.get('/polls/', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('Polls');
+    collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
         if (e) return next(e)
         res.send(results)
     })
 })
 
-router.post('/collections/:collectionName', function(req, res, next) {
+/*router.post('/collections/:collectionName', function(req, res, next) {
     req.collection.insert(req.body, {}, function(e, results){
         if (e) return next(e)
         res.send(results)
@@ -47,4 +46,4 @@ router.delete('/collections/:collectionName/:id', function(req, res, next) {
         if (e) return next(e)
         res.send((result===1)?{msg:'success'}:{msg:'error'})
     })
-})
+})*/
