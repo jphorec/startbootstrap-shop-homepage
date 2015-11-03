9f7a73e2434b0fe61da9422bbd181d1aa9142ad3
josh.horecny <jphorec@gmail.com>
2015-11-03 15:03:49 -0600

    Added Angular Junk and now working index page

--- routes/restservice.js
+++ routes/restservice.js
@@ -5,44 +5,44 @@ var express = require('express');
 var router = express.Router();
 
 
-app.param('collectionName', function(req, res, next, collectionName){
+router.param('collectionName', function(req, res, next, collectionName){
     req.collection = db.collection(collectionName)
     return next()
 })
 
-app.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.send('Must Include Collection Name')
 })
 
-app.get('/collections/:collectionName', function(req, res, next) {
+router.get('/collections/:collectionName', function(req, res, next) {
     req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
         if (e) return next(e)
         res.send(results)
     })
 })
 
-app.post('/collections/:collectionName', function(req, res, next) {
+router.post('/collections/:collectionName', function(req, res, next) {
     req.collection.insert(req.body, {}, function(e, results){
         if (e) return next(e)
         res.send(results)
     })
 })
 
-app.get('/collections/:collectionName/:id', function(req, res, next) {
+router.get('/collections/:collectionName/:id', function(req, res, next) {
     req.collection.findById(req.params.id, function(e, result){
         if (e) return next(e)
         res.send(result)
     })
 })
 
-app.put('/collections/:collectionName/:id', function(req, res, next) {
+router.put('/collections/:collectionName/:id', function(req, res, next) {
     req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
         if (e) return next(e)
         res.send((result===1)?{msg:'success'}:{msg:'error'})
     })
 })
 
-app.delete('/collections/:collectionName/:id', function(req, res, next) {
+router.delete('/collections/:collectionName/:id', function(req, res, next) {
     req.collection.removeById(req.params.id, function(e, result){
         if (e) return next(e)
         res.send((result===1)?{msg:'success'}:{msg:'error'})
