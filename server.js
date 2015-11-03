fba122636028914e18374b452d4e90354ed9e733
josh.horecny <jphorec@gmail.com>
2015-11-03 14:52:38 -0600

    Added Angular Junk and now working index page

--- server.js
+++ server.js
@@ -30,48 +30,13 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.param('collectionName', function(req, res, next, collectionName){
-  req.collection = db.collection(collectionName)
-  return next()
-})
 
-app.get('/init/', function(req, res, next) {
+app.use('/users', routes/restservice);
+
+
+app.get('/', function(req, res, next) {
   res.render('./index')
 })
 
-app.get('/collections/:collectionName', function(req, res, next) {
-  req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
-    if (e) return next(e)
-    res.send(results)
-  })
-})
-
-app.post('/collections/:collectionName', function(req, res, next) {
-  req.collection.insert(req.body, {}, function(e, results){
-    if (e) return next(e)
-    res.send(results)
-  })
-})
-
-app.get('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.findById(req.params.id, function(e, result){
-    if (e) return next(e)
-    res.send(result)
-  })
-})
-
-app.put('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
-    if (e) return next(e)
-    res.send((result===1)?{msg:'success'}:{msg:'error'})
-  })
-})
-
-app.delete('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.removeById(req.params.id, function(e, result){
-    if (e) return next(e)
-    res.send((result===1)?{msg:'success'}:{msg:'error'})
-  })
-})
 
 app.listen(process.env.PORT || 3000)
