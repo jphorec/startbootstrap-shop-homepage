f2c10c0d0d16400923ccbad8bb524eac0f835727
josh.horecny <jphorec@gmail.com>
2015-11-05 10:00:17 -0600

    Removed full service URL

--- js/services.js
+++ js/services.js
@@ -2,7 +2,7 @@
  * Created by josh.horecny on 11/2/15.
  */
 angular.module('pollApp.services', []).factory('Poll', function($resource) {
-    return $resource('https://bench-project.herokuapp.com/collections/Polls/:id', { id: '@_id' }, {
+    return $resource('/collections/Polls/:id', { id: '@_id' }, {
         update: {
             method: 'PUT'
         }
