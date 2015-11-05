f314ab97ca728ddacbc6a84e310ef50ae51b2d5f
josh.horecny <jphorec@gmail.com>
2015-11-05 15:27:37 -0600

    Added images

--- js/services.js
+++ js/services.js
@@ -1,7 +1,7 @@
 /**
  * Created by josh.horecny on 11/2/15.
  */
-angular.module('pollApp.services', []).factory('Poll', function($resource) {
+angular.module('pollApp.services').factory('Poll', function($resource) {
     return $resource('/collections/Polls/:id', { id: '@_id' }, {
         'get':    {method:'GET'},
         'save':   {method:'POST', isArray:true},
