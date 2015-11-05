c3e821bb89429451c673575c98b17f017c482d19
josh.horecny <jphorec@gmail.com>
2015-11-05 14:47:46 -0600

    Added images

--- js/services.js
+++ js/services.js
@@ -3,10 +3,8 @@
  */
 angular.module('pollApp.services', []).factory('Poll', function($resource) {
     return $resource('/collections/Polls/:id', { id: '@_id' }, {
-        'get':    {method:'GET'},
-        'save':   {method:'POST'},
-        'query':  {method:'GET'},
-        'remove': {method:'DELETE'},
-        'delete': {method:'DELETE'}
+        update: {
+            method: 'PUT'
+        }
     });
 });
