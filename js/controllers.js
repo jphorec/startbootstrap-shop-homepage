ad09d87ec9fa09ed172c8b422547584dad20c8b2
josh.horecny <jphorec@gmail.com>
2015-11-06 13:52:31 -0600

    Added images

--- js/controllers.js
+++ js/controllers.js
@@ -4,7 +4,7 @@
 angular.module('pollApp.controllers', []).controller('PollListController', function($scope, $state, $window, Poll) {
     $scope.polls = Poll.query(); //fetch all polls. Issues a GET to /collections/Polls
     $scope.submitVote=function(){
-        alert($scope.polls);
+        alert($scope.poll);
     }
 
 
